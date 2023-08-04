eb7bb85f0a447f70b8760449f43177996d18e410
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-08-04 13:43:16 +0300

    Bug fixed. Successcully connected to keycloak

--- api-gateway/src/main/java/com/jirafik/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jirafik/gateway/ApiGatewayApplication.java
@@ -10,10 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
-    //Connect attempt to [localhost/127.0.0.1:8080] failed
-
     public static void main(String[] args) {
-        log.info("Initializing v7 cats-gateway: replaced issuer-uri");
+        log.info("Initializing v7 cats-gateway: reconfigured issuer uri");
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
